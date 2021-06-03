45322ed868ac7966f05634fe052c45d578c0d12a
spalpeen <oxybimboo@gmail.com>
2021-06-04 01:25:44 +0200

    delete

--- Moroz_Vladislav_dz_4/task_4_5.py
+++
@@ -1,5 +0,0 @@
-import utils
-
-if __name__ == '__main__':
-    import sys
-    exit(utils.currency_rates(sys.argv[1:]))
--- Moroz_Vladislav_dz_5/task_5_5.py
+++
@@ -1,3 +0,0 @@
-src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-
-print([i for i in src if src.count(i) == 1])
